69db63d9ff82bf4bccf2eb375b92d7235f7b22ad
Siddharth Talwekar <siddharthtalwekar@gmail.com>
2019-11-18 22:22:43 -0500

    Added app js

---
+++ app.js
@@ -0,0 +1,24 @@
+const yargs = require('yargs');
+const generateMain = require('./generateMain');
+
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+    command: 'generate',
+    describe: 'Generate source code from JSON',
+    builder: {
+        jsonFile: {
+            describe: 'Metadata JSON file',
+            demandOption: true,
+            type: 'String'
+        }
+    },
+    handler(argv) {
+        console.log(argv.jsonFile);
+        generateMain.generate(argv.jsonFile);
+    }
+});
+
+yargs.parse();
