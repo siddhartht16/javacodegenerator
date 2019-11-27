a38e50518f231b96a77efd22ec595920955271ae
Siddharth Talwekar <siddharthtalwekar@gmail.com>
2019-11-26 23:33:32 -0500

    Refactored utils

--- utils.js
+++ utils.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 const rimraf = require("rimraf");
 const chalk = require("chalk");
-const pluralize = require("pluralize");
-const _ = require("lodash");
+const ejs = require("ejs");
+const { startCase, toLower, isEmpty } = require("lodash");
 const constants = require("./constants");
 
 const removeDirectory = dir_name => rimraf.sync(dir_name);
+
+const cleanUp = () => removeDirectory(constants.OUTPUT_DIRECTORY);
 
 const createDirectory = dir_name => {
     if (!fs.existsSync(dir_name)) {
@@ -17,6 +19,36 @@ const createFile = (path, contents) => {
     fs.writeFileSync(path, contents);
 };
 
+const generateFileUsingEJS = (templateFile, filename, filepath, data) => {
+    logMessage(`Start processing for ${filename}`);
+    const dataObj = {};
+    dataObj[constants.TEMPLATE_PLACEHOLDERS.CLASS_OBJECT] = data;
+    ejs.renderFile(templateFile, dataObj, {}, function(err, str) {
+        // console.log(str);
+        createFile(filepath, str);
+        logSuccessMessage(`End processing for ${filename}`);
+    });
+};
+
+const getJSONData = jsonFilePath => {
+    const dataBuffer = fs.readFileSync(jsonFilePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+};
+
+const getJavaFileName = filename => `${filename}.java`;
+
+const getControllerFilesPath = packageName =>
+    `${constants.OUTPUT_DIRECTORY}/${packageName}/${constants.CONTROLLERS_DIRECTORY}/`;
+
+const getModelFilesPath = packageName => `${constants.OUTPUT_DIRECTORY}/${packageName}/${constants.MODELS_DIRECTORY}/`;
+
+const getRepositoryFilesPath = packageName =>
+    `${constants.OUTPUT_DIRECTORY}/${packageName}/${constants.REPOSITORIES_DIRECTORY}/`;
+
+const getServiceFilesPath = packageName =>
+    `${constants.OUTPUT_DIRECTORY}/${packageName}/${constants.SERVICES_DIRECTORY}/`;
+
 const handleError = (error, message) => {
     logError(message);
     throw error;
@@ -26,39 +58,74 @@ const logMessage = message => console.log(chalk.blue(message));
 const logSuccessMessage = message => console.log(chalk.green(message));
 const logError = message => console.log(chalk.inverse.red(message));
 
+const getKeyValueFromJSON = (jsonObject, key, defaultValue = ``) => {
+    if (isEmpty(jsonObject)) return defaultValue;
+    return key && key in jsonObject ? jsonObject[key] : defaultValue;
+};
+
 const getTypeValue = type => {
-    return type.toLowerCase() in constants.TYPE_MAPPING ? constants.TYPE_MAPPING[type.toLowerCase()] : type;
+    return type && type.toUpperCase() in constants.TYPE_MAPPING ? constants.TYPE_MAPPING[type.toUpperCase()] : type;
+};
+
+const getBoxTypeValue = type => {
+    return type && type.toUpperCase() in constants.BOX_TYPE_MAPPING
+        ? constants.BOX_TYPE_MAPPING[type.toUpperCase()]
+        : type;
+};
+
+const getDataTypeValue = type => {
+    return type && type.toUpperCase() in constants.DATA_TYPE_MAPPING
+        ? constants.DATA_TYPE_MAPPING[type.toUpperCase()]
+        : type;
+};
+
+const getCollectionTypeValue = type => {
+    return type && type.toUpperCase() in constants.COLLECTION_TYPE_MAPPING
+        ? constants.COLLECTION_TYPE_MAPPING[type.toUpperCase()]
+        : type;
+};
+
+const getMapTypeValue = type => {
+    return type && type.toUpperCase() in constants.MAP_TYPE_MAPPING
+        ? constants.MAP_TYPE_MAPPING[type.toUpperCase()]
+        : type;
 };
 
 const getAccessModifierValue = accessModifier => {
-    return accessModifier.toLowerCase() in constants.ACCESS_MODIFIER_MAPPING
-        ? constants.ACCESS_MODIFIER_MAPPING[accessModifier.toLowerCase()]
+    return accessModifier && accessModifier.toUpperCase() in constants.ACCESS_MODIFIER_MAPPING
+        ? constants.ACCESS_MODIFIER_MAPPING[accessModifier.toUpperCase()]
         : accessModifier;
 };
 
-const getJPARelationshipValue = relationShip => {
-    return relationShip.toLowerCase() in constants.JPA_RELATIONSHIP_MAPPING
-        ? constants.JPA_RELATIONSHIP_MAPPING[relationShip.toLowerCase()]
-        : relationShip;
+const capitalizeFirstLetter = input => startCase(toLower(input));
+
+const getRelationShipsObject = (className, relationShips = {}) => {
+    return className && className.toLowerCase() in relationShips ? relationShips[className.toLowerCase()] : [];
 };
-
-const capitalizeFirstLetter = input => _.startCase(_.toLower(input));
-
-const singularizeClassName = className => pluralize.singular(className);
-const pluralizeClassName = className => pluralize.plural(className);
 
 module.exports = {
     removeDirectory,
     createDirectory,
+    cleanUp,
     createFile,
+    generateFileUsingEJS,
+    getJSONData,
+    getJavaFileName,
+    getControllerFilesPath,
+    getModelFilesPath,
+    getRepositoryFilesPath,
+    getServiceFilesPath,
     handleError,
     logError,
     logMessage,
     logSuccessMessage,
+    getKeyValueFromJSON,
     getTypeValue,
+    getBoxTypeValue,
+    getDataTypeValue,
+    getCollectionTypeValue,
+    getMapTypeValue,
     getAccessModifierValue,
     capitalizeFirstLetter,
-    singularizeClassName,
-    pluralizeClassName,
-    getJPARelationshipValue
+    getRelationShipsObject
 };
