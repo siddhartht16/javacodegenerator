48bee3e50c91e70286e4af70937fd4509139ca9d
Siddharth Talwekar <siddharthtalwekar@gmail.com>
2019-11-26 23:31:26 -0500

    Added file to define messages

---
+++ messages.js
@@ -0,0 +1,7 @@
+const CLASS_NAME_REQUIRED = `Missing class name in JSON`;
+const INVALID_JSON_FILE = `Input JSON File does not exists.`;
+
+module.exports = {
+    INVALID_JSON_FILE,
+    CLASS_NAME_REQUIRED
+};
