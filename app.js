22ab2b8b5f4e3ee3c460a09301db11c82273f6fa
Siddharth Talwekar <siddharthtalwekar@gmail.com>
2019-12-06 15:55:15 -0500

    Added mandatory param to specify output folder.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require("yargs");
 const generateMain = require("./generateMain");
+const utils = require("./common/utils");
 
 // Customize yargs version
 yargs.version("1.1.0");
@@ -13,11 +14,16 @@ yargs.command({
             describe: "Metadata JSON file",
             demandOption: true,
             type: "String"
+        },
+        output: {
+            describe: "Output path",
+            demandOption: true,
+            type: "String"
         }
     },
     handler(argv) {
-        console.log(argv.jsonFile);
-        generateMain.generate(argv.jsonFile);
+        utils.logMessage(`JSON File: ${argv.jsonFile}, Output: ${argv.output}`);
+        generateMain.generate(argv.jsonFile, argv.output);
     }
 });
 
